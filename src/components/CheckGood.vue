808220fc9a2668eefcfdec8706d4851b235401ef
刘博一 <19373427@buaa.edu.cn>
2021-11-09 20:16:33 +0800

    v1.3

--- src/components/CheckGood.vue
+++ src/components/CheckGood.vue
@@ -8,8 +8,8 @@
           name="file"
           :multiple="false"
           action="http://127.0.0.1:5000/ImageUpload"
-          accept=".jpg,.png"
           :headers="headers"
+          accept=".jpg,.png,.jpeg"
           :data="uploadData"
           @change="handleChange"
           style="float: right"
@@ -32,38 +32,54 @@
       <div id="carousel">
         <a-carousel :after-change="onChange" v-if="imgList.length!==0" autoplay>
           <div v-for="v in imgList" :key="v.value" style="">
-            <img :src="v.src" alt="" style="height: 180px;margin: 0 auto">
+            <img :src="v.picUrl" alt="" style="height: 220px;margin: 0 auto">
+<!--            可以添加的功能：删除图片（初步想法：双击删除）-->
           </div>
         </a-carousel>
       </div>
     </div>
+    <br>
+
+    <a-descriptions title="" bordered :column="2">
+      <a-descriptions-item label="商品名">
+        {{this.goodName}}
+      </a-descriptions-item>
+      <a-descriptions-item label="商品类别">
+        {{this.goodType}}
+      </a-descriptions-item>
+      <a-descriptions-item label="卖家">
+        {{this.sellerName}}
+      </a-descriptions-item>
+      <a-descriptions-item label="价格">
+        ￥{{this.goodPrice}}
+      </a-descriptions-item>
+      <a-descriptions-item label="商品描述" :span="2">
+        {{this.goodDescription}}
+      </a-descriptions-item>
+    </a-descriptions>
 
 
   </div>
 </template>
 
 <script>
-let imgList = [
-  {
-    value: '1',
-    src: 'https://i.52112.com/icon/256/20131031/298/6505.png',
-  },
-  {
-    value: '1',
-    src: 'https://img.zcool.cn/community/01f21a5875fddca801219c774bd034.jpg@1280w_1l_2o_100sh.jpg',
-  }
-]
-
+let imgList = []
 
 export default {
   data() {
     return {
       imgList,
+      goodName: '',
+      goodType: '',
+      sellerName: '',
+      goodPrice: '',
+      goodDescription: '',
       headers: {
         authorization: 'authorization-text',
       },
       uploadData: {
-        "userId": this.$store.state.userID
+        "userId": this.$store.state.userID,
+        "goodId": this.$store.state.goodId
       }
     }
   },
@@ -79,11 +95,29 @@ export default {
         console.log(info.file, info.fileList);
       }
       if (info.file.status === 'done') {
-        this.$message.success(`${info.file.name} 上传成功!`);
+        this.$message.success(`${info.file.name} 上传成功！`);
       } else if (info.file.status === 'error') {
-        this.$message.error(`${info.file.name} 上传失败!`);
+        this.$message.error(`${info.file.name} 上传失败！仅支持.jpg和.png格式文件！`);
       }
     },
+  },
+  created() {
+    let that = this
+    this.$axios.post(this.$store.state.ip + "QueryPictures", {
+      "goodId": this.$store.state.goodId
+    }).then((response) => {
+      console.log(response.data)
+      that.imgList = response.data
+    })
+    this.$axios.post(this.$store.state.ip + "QueryGoodsInfo", {
+      "goodId": this.$store.state.goodId
+    }).then((response) => {
+      that.goodName = response.data.goodName
+      that.goodPrice = response.data.goodPrice
+      that.goodType = response.data.goodType
+      that.sellerName = response.data.sellerName
+      that.goodDescription = response.data.goodDescription
+    })
   },
 };
 </script>
@@ -91,16 +125,11 @@ export default {
 /* For demo */
 .ant-carousel >>> .slick-slide {
   text-align: center;
-  height: 180px;
+  height: 220px;
   line-height: 160px;
-  background: grey;
+  background: wheat;
   overflow: hidden;
 }
-
-.ant-carousel >>> .slick-slide h3 {
-  color: red;
-}
-
 
 
 #carousel {
